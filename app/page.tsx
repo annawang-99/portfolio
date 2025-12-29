9f5ef6805e1e0a75cf43016e998f30304a67f256
annawang-99 <anna.inbox.3838@gmail.com>
2025-12-29 00:11:17 -0500

    feat: removed fade-in effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,12 @@
-"use client";
-
 import About from "./pages/about-me";
 import Projects from "./pages/projects";
-import { motion } from "framer-motion";
-
-function Fade({ children }: { children: React.ReactNode }) {
-  return (
-    <motion.section
-      className="min-h-screen"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      viewport={{ amount: 0.5, once: false }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
-    >
-      {children}
-    </motion.section>
-  );
-}
 
 export default function Home() {
   return (
     <div className="bg-black">
       <div className="scroll-bar" />
-      <Fade>
         <About />
-      </Fade>
-      <Fade>
         <Projects />
-      </Fade>
     </div>
   );
 }
