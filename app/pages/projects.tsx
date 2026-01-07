7d93f4a6bfb41f8062d50e45c733ed577aa2e321
annawang-99 <anna.inbox.3838@gmail.com>
2026-01-07 16:06:06 -0500

    feat: mobile responsive

--- app/pages/projects.tsx
+++ app/pages/projects.tsx
@@ -13,8 +13,9 @@ export default function Projects({ setActiveTab }: ProjectsProps) {
   
     return (
     <div className="space-y-6 w-full lg:max-w-5xl 2xl:max-w-6xl">
-        <p className="text-zinc-500 text-lg max-w-[500px] leading-relaxed mb-12">
-            Several projects I've built in the past.
+        <p className="text-zinc-500 text-lg max-w-[800px] leading-relaxed mb-12">
+            Several projects I've built in the past. Visit Github Repos to explore the tech stack, 
+            architectural decisions, and full project documentation.
         </p>
         {projects.map((proj, i) => {
         const isExpanded = expandedProject === i;
@@ -33,59 +34,6 @@ export default function Projects({ setActiveTab }: ProjectsProps) {
                 </span>
             </button>
 
-            {/* <AnimatePresence>
-                {isExpanded && (
-                <motion.div initial={{ height: 0, opacity: 0 }} animate={{ height: "auto", opacity: 1 }}
-                    exit={{ height: 0, opacity: 0 }} transition={{ duration: 0.4, ease: [0.22, 1, 0.36, 1] }}
-                    className="overflow-hidden">
-                <div className="pt-8 pb-4 grid gap-8 pointer-events-auto">
-                    <div>
-                        <div className='mb-6'>
-                            <p className="text-lg text-zinc-300 leading-relaxed">
-                                {proj.description}
-                            </p>
-                            {proj.why && (
-                                <p className="text-lg text-zinc-300 leading-relaxed">
-                                    <span className='italic'>why?&nbsp;&nbsp;</span>{proj.why}
-                                </p>
-                            )}
-                            {proj.notes && (
-                                <p className="text-lg text-zinc-500 leading-relaxed">
-                                    side note:&nbsp;{proj.notes}
-                                </p>
-                            )}
-                        </div>
-                        
-                        {!!proj.images?.length && (
-                            <div className="mb-6 flex space-x-4 overflow-x-auto" onClick={(e) => e.stopPropagation()}>
-                            {proj.images.map((src: string, idx: number) => (
-                                <a key={idx} href={proj.siteUrl || proj.repoUrl} target="_blank" rel="noopener noreferrer"
-                                    className="relative min-w-[220px] h-[140px] 2xl:min-w-[300px] 2xl:h-[220px] border border-white transition-colors cursor-none group/img">
-                                    <Image src={src} alt={`${proj.title}`} fill className="object-cover group-hover/img:opacity-80 transition-opacity" />
-                                </a>
-                            ))}
-                            </div>
-                        )}
-                        <div className="flex gap-2">
-                            {proj.techStack.map(tech => (
-                                <span key={tech} className="px-3 py-1 bg-white/5 border border-white/10 rounded-full text-[10px] uppercase tracking-wider text-zinc-400">
-                                    {tech}
-                                </span>
-                            ))}
-                            <div className="flex items-center gap-4 justify-end ml-4">
-                                {proj.siteUrl && (
-                                    <a href={proj.siteUrl} target="_blank" rel="noopener noreferrer" 
-                                    className="cursor-none text-white hover:text-zinc-500 transition-colors text-sm font-bold uppercase tracking-widest">Live Site ↗</a>
-                                )}
-                                <a href={proj.repoUrl} target="_blank" rel="noopener noreferrer" 
-                                className="cursor-none text-white hover:text-zinc-500 transition-colors text-sm font-bold uppercase tracking-widest">Github Repo ↗</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </motion.div>
-                )}
-            </AnimatePresence> */}
             <AnimatePresence>
                 {isExpanded && (
                 <motion.div initial={{ height: 0, opacity: 0 }} animate={{ height: "auto", opacity: 1 }}
@@ -137,10 +85,10 @@ export default function Projects({ setActiveTab }: ProjectsProps) {
                             <div className="flex items-center gap-6 md:gap-4">
                                 {proj.siteUrl && (
                                     <a href={proj.siteUrl} target="_blank" rel="noopener noreferrer" 
-                                    className="cursor-none text-white hover:text-zinc-500 transition-colors text-[11px] md:text-sm font-bold uppercase tracking-widest ml-1">Live Site ↗</a>
+                                    className="cursor-none text-white hover:text-zinc-500 transition-colors text-[11px] md:text-sm font-bold uppercase tracking-widest ml-1">Live Site &rarr;</a>
                                 )}
                                 <a href={proj.repoUrl} target="_blank" rel="noopener noreferrer" 
-                                className="cursor-none text-white hover:text-zinc-500 transition-colors text-[11px] md:text-sm font-bold uppercase tracking-widest">Github Repo ↗</a>
+                                className="cursor-none text-white hover:text-zinc-500 transition-colors text-[11px] md:text-sm font-bold uppercase tracking-widest">Github Repo &rarr;</a>
                             </div>
                         </div>
                     </div>
@@ -163,6 +111,7 @@ export default function Projects({ setActiveTab }: ProjectsProps) {
                     {expandedProject === 999 ? "-" : "+"}
                 </span>
             </button>
+            
 
             <AnimatePresence>
                 {expandedProject === 999 && (
@@ -170,14 +119,14 @@ export default function Projects({ setActiveTab }: ProjectsProps) {
                     exit={{ height: 0, opacity: 0 }} transition={{ duration: 0.4, ease: [0.22, 1, 0.36, 1] }}
                     className="overflow-hidden">
                 <div className="pt-8 pb-4 grid gap-6 pointer-events-auto">
-                    <p className="text-lg text-zinc-300 leading-relaxed mb-2">
+                    <p className="text-base md:text-lg text-zinc-300 leading-relaxed mb-2">
                         My first portfolio was built with pure HTML/CSS, my second with React (Next.js) and Tailwind.
                     </p>
                     <div className="flex items-center gap-4">
                         <a href="https://annaw-99.github.io/web/" target="_blank" rel="noopener noreferrer" 
-                        className="text-white hover:text-zinc-500 transition-colors duration-300 ease-in-out text-sm font-bold uppercase tracking-widest cursor-none">v1.0 Site ↗</a>
+                        className="cursor-none text-white hover:text-zinc-500 transition-colors text-[11px] md:text-sm font-bold uppercase tracking-widest ml-1">v1.0 Site &rarr;</a>
                         <a href="https://annaw99.vercel.app/" target="_blank" rel="noopener noreferrer" 
-                        className="text-white hover:text-zinc-500 transition-colors duration-300 ease-in-out text-sm font-bold uppercase tracking-widest cursor-none">v2.0 Site ↗</a>
+                        className="cursor-none text-white hover:text-zinc-500 transition-colors text-[11px] md:text-sm font-bold uppercase tracking-widest">v2.0 Site &rarr;</a>
                     </div>
                 </div>
                 </motion.div>
